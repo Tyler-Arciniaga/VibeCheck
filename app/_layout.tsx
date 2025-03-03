c054ee0e2f94c5c021aeb20ee80ffb4f4f75bd95
Tyler Arciniaga <tylerarc@umich.edu>
2025-03-03 03:10:47 -0500

    Starting to set up basic file structure, dividing main app with auth flow

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
 import { Stack } from "expo-router";
 
+
 export default function RootLayout() {
+  const isLoggedIn: boolean = true;
+  {/**Will involve useSupabase later on but for right now just testing general restrictions based on user sign in status */}
   return (
     <Stack>
-    <Stack.Screen name="(tabs)" />
-
+      <Stack.Screen name="(app)"/>
     </Stack>
-  
   );
 }
