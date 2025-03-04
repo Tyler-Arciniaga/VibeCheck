0dd19f5c496c68735e0eb5463da56c1e09d05496
Tyler Arciniaga <tylerarc@umich.edu>
2025-03-03 19:31:09 -0500

    Getting error with index in (auth) saying not existent but clearly in file structure.

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,13 +1,14 @@
 import { Redirect, Stack } from "expo-router";
 
-
 export default function AppLayout() {
-  const isLoggedIn: boolean = true;
-  {/**Will involve useSupabase/or FireBase later on but for right now just testing general restrictions based on user sign in status */}
-
-  if (!isLoggedIn) {
-    return <Redirect href="../(auth)/welcome"/>
+  const isLoggedIn: boolean = false;
+  {
+    /**Will involve useSupabase/or FireBase later on but for right now just testing general restrictions based on user sign in status */
   }
 
-  return <Stack />
+  if (!isLoggedIn) {
+    return <Redirect href="../(auth)/index" />;
+  }
+
+  return <Stack />;
 }
